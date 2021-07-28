7bebc9ea589b9e1a6f3d103d0ef89091a388df2b
TeacherSamanthaSilva <samanthaadelinecordova@outlook.com>
2021-07-27 22:28:43 -0300

    javascript
    Nesse commit dei inicio ao javascipt do sistema financeiro

--- script.js
+++ script.js
@@ -1,1 +1,53 @@
 
+const Modal = {
+    open(){
+
+        document.querySelector('.model-overlay')
+        .classList.add('active')
+
+    },
+    close(){
+
+        document.querySelector('.model-overlay')
+        .classList.remove('active')
+
+    }
+
+    
+}
+
+const transction = [{
+    id = 1,
+    description:'Luz',
+    amount :-50000,
+    date:'23/01/2021'
+},{
+    id = 2,
+    description:'website',
+    amount :-5000000,
+    date:'23/01/2021'
+},{
+    id = 3,
+    description:'internet',
+    amount :-20000,
+    date:'23/01/2021'
+}]
+
+const Transaction = {
+    incomes(){
+
+    },
+    expendes(){
+
+    }, 
+    total(){
+
+    }
+}
+
+const DOM ={
+    innerHTMLTransaction(){
+        const html = 
+    }
+}
+
